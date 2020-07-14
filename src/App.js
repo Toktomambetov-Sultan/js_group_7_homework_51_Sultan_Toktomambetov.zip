a8a1f211c76373b8a0ef3bda823e3f8461212541
Toktomambetov Sultan <sultan.toktomambetov.04@bk.ru>
2020-07-14 23:08:25 +0600

    changed App.js, added Coin.js component, made state.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Coin from './components/coin';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state={
+    coins: [],
+    addingCoins: [],
+  }
+  render(){
+    return(
+      <div className="App">
+        <button>
+          new numbers
+        </button>
+        <ul>
+          <Coin></Coin>
+        </ul>
+      </div>
+    )
+  }
 }
 
 export default App;
